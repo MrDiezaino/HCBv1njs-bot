9a78ccbaf7de68392e8422a4ece0ba12ba397b9d
MrDiezaino <>
2021-12-26 22:13:59 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const token = process.env['token']
+
+client.on("ready", () => {
+    console.log("Estoy listo!");
+ });
+ 
+ client.on("message", (message) => {
+   if(message.content.startsWith("ping")) {
+     message.channel.send("pong!");
+   }
+ 
+ });
+ 
+ client.login(token);
