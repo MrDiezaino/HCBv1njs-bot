ce503d913d6abfee60f2e6fff56e1372496b367c
MrDiezaino <72072803+MrDiezaino@users.noreply.github.com>
2021-12-26 23:34:24 +0000

    Creado un Command Handler, y cambiado el comando ping para que funcione con él.

---
+++ comandos/ping.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // Propiedades del Comando
+  nombre: "ping",
+  alias: ["p"],
+  descripcion: "Este comando te muestra el Ping",
+  run: (client, message, args) => {
+    return message.channel.send("Pong")
+  }
+}
--- index.js
+++ index.js
@@ -2,15 +2,47 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const token = process.env['token']
 
+// Command Handler
+const fs = require("fs");
+
+// Con client.comandos creamos una coleccion donde almacenaremos la información de nuestros comandos.
+client.comandos = new Discord.Collection()
+
+// Variable que contiene los nombres de los archivos de la carpeta
+let archivos = fs.readdirSync("./comandos").filter((f) => f.endsWith(".js"))
+
+// Recorremos los elementos del Array archivos.
+// Básicamente aquí obtendremos las propiedades que tiene el comando en su propia archivo.
+for (var i of archivos) {
+  let comando = require("./comandos/"+i)
+  // Añadimos el comando a la colección, esto nos pedirá una clave y un valor.
+  // La clave será el nombre del comando ("comando.nombre") y el valor será básicamente el objeto entero de comando.
+  client.comandos.set(comando.nombre, comando)
+  console.log(i + " fue cargado correctamente.")
+}
+
 client.on("ready", () => {
-    console.log("Estoy listo!");
+    console.log(`Bot ${client.user.tag} iniciado.`);
  });
- 
- client.on("message", (message) => {
-   if(message.content.startsWith("ping")) {
-     message.channel.send("pong!");
-   }
- 
- });
+
+
+client.on("message", async message => {
+
+    if (message.author.bot) return;
+
+
+let prefix =  "!"
+
+let args = message.content.slice(prefix.length).trim().split(/ +/g)
+let command = args.shift().toLowerCase();
+// El método get de client.comandos recibe la clave del comando, que básicamente es command
+// Mediante el .find, buscamos si el comando se ha llamado usando alguno de sus alias.
+
+let cmd = client.comandos.get(command) || client.comandos.find((c) => c.alias.includes(command))
+if (cmd) {
+  return cmd.run(client, message, args)
+}
+
+});
  
  client.login(token);
